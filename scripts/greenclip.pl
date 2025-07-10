2191022252d366247b9d3a72a4806ff61aee9bf8
krishna <krishna404@yandex.com>
2025-07-11 00:06:34 +0530

    update greenclip

--- scripts/greenclip.pl
+++ scripts/greenclip.pl
@@ -6,4 +6,5 @@ my $prompt = "Greenclip »";
 chomp(my $chosen = `greenclip print | dmenu -p "$prompt" -l 15 -i`);
 unless ($chosen) { exit };
 
+$chosen =~ s/ /\n/g;
 system("printf '%s' '$chosen' | xclip -selection clipboard");
