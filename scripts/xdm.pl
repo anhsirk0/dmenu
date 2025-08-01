6fe2be2926896156453d151a5af3c65ddc8045fe
krishna <krishna404@yandex.com>
2025-08-01 16:53:04 +0530

    add xdm windows script

---
+++ scripts/xdm.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use warnings;
+
+my @desktop_list = split "\n", `wmctrl -d`;
+my %desktops = ("-1" => "-"); # WORKAROUND: sticky windows (-1)
+
+for (@desktop_list) {
+    my ($index, $active, $name) = $_ =~ m/^(\d+)\s+(.) .* (.*?)$/;
+    $desktops{$index} = $name;
+}
+
+my @window_list = split "\n", `wmctrl -l`;
+my $windows = "";
+my $count = 0;
+
+for my $i (1 .. scalar @window_list) {
+    my ($idx, $h, $name) = $window_list[$i -1] =~ m/^.*?\s+(\d+) (.*?) (.*?)$/;
+    if ($name =~ m/\[\d+%\]/) {
+        $count++;
+        $windows .= "$count) $name\n";        
+    }
+}
+chomp $windows;
+
+my $prompt = "XDM »";
+chomp(my $chosen = `echo "$windows" | dmenu -p "$prompt" -l 15 -i`);
+exit unless ($chosen);
+
+my ($num) = $chosen =~ m/^(\d+).*$/;
+my ($window_id) = $window_list[$num -1] =~ m/^(.*?)\s+.*$/;
+
+exit system("wmctrl -i -a $window_id");
